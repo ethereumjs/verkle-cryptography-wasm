64a8b18d62c0a2caf077461a5b3b31d404041aed
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-12-15 20:46:17 +0000

    upload new usage of pedersen_hash

--- js_code/pedersen.js
+++ js_code/pedersen.js
@@ -1,8 +1,9 @@
 
 var wasm = require('../pkg/rust_verkle_wasm');
 
-let ff_value_16_byte = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]);
+let zeroes = new Uint8Array(64);
 
 // This function accepts uint8 arrays or integers which are less than 128 bit/16 bytes
-let hash = wasm.pedersen_hash([ff_value_16_byte, 1]);
-console.log(hash);
+let hashBytes = wasm.pedersen_hash(zeroes);
+let hashHex = Buffer.from(hashBytes).toString('hex');
+console.log(hashHex);
