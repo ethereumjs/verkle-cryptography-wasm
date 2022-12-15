bf5685686000a5cea089c49852c96bd1e586736d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-12-15 20:48:16 +0000

    update comment

--- js_code/pedersen.js
+++ js_code/pedersen.js
@@ -3,7 +3,8 @@ var wasm = require('../pkg/rust_verkle_wasm');
 
 let zeroes = new Uint8Array(64);
 
-// This function accepts uint8 arrays or integers which are less than 128 bit/16 bytes
+// This function accepts a 64 byte array. 
+// This should be the address concatenated with the tree index 
 let hashBytes = wasm.pedersen_hash(zeroes);
 let hashHex = Buffer.from(hashBytes).toString('hex');
 console.log(hashHex);
