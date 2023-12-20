bdbd24d2f057683c971cf05ddd863e2072b4e8e1
Gabriel Rocheleau <contact@rockwaterweb.com>
2023-12-19 23:40:15 -0500

    format: run prettier

--- bindings/js_code/pedersen.js
+++ bindings/js_code/pedersen.js
@@ -1,10 +1,9 @@
-
-var wasm = require('../pkg/rust_verkle_wasm');
+var wasm = require("../pkg/rust_verkle_wasm");
 
 let zeroes = new Uint8Array(64);
 
-// This function accepts a 64 byte array. 
-// This should be the address concatenated with the tree index 
+// This function accepts a 64 byte array.
+// This should be the address concatenated with the tree index
 let hashBytes = wasm.pedersen_hash(zeroes);
-let hashHex = Buffer.from(hashBytes).toString('hex');
+let hashHex = Buffer.from(hashBytes).toString("hex");
 console.log(hashHex);
