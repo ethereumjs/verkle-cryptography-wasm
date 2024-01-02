b478e05dccc26930a9ce48f45aa9c47c4cf2d5e2
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-01-02 08:35:35 -0500

    Clean up configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: [
+    '.eslintrc.js',
+    'dist',
+    'jest.config.js',
+    'node_modules',
+    'rust-wasm',
+    'scripts',
+  ],
   rules: {
     "comma-spacing": ["error", { before: false, after: true }],
     "no-unused-vars": "off",
---
+++ scripts/copy-wasm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir dist/rust-wasm
+mkdir dist/rust-wasm/outputs
+cp -r src/rust-wasm/outputs dist/rust_wasm
