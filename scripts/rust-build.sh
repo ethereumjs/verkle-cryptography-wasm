6a74471eda439f5926f46fdfabbed058d523f308
Holger Drewes <Holger.Drewes@gmail.com>
2024-01-24 16:19:43 +0100

    Simplify rust-build.sh script structure and output paths, unify to have cjs/esm based output folder names, remove external scripts

--- scripts/rust-build.sh
+++ scripts/rust-build.sh
@@ -21,29 +21,59 @@ function run_or_fail {
     exit $status
   fi
 }
+function run_if_available {
+  if command -v "$1" >/dev/null 2>&1; then
+    "$@"
+  else
+    echo "$1 is not installed. Please install it to use this feature." >&2
+  fi
+}
 
-cd src/rust-wasm
+echo "Checking Rust/WASM requirements..."
 
 require_command jq
 require_command cargo
 require_command wasm-bindgen
 check_installed wasm-opt
 
-self_path=$(dirname "$(readlink -f "$0")")
-export pname=$(cargo read-manifest | jq -r '.name')
-export CARGO_TARGET_DIR=$self_path/target
+SELF_DIR=$(pwd)
+RUST_DIR=$SELF_DIR/src/rust-wasm
 
-rm -rf $self_path/outputs >/dev/null 2>&1
-rm -rf $self_path/result >/dev/null 2>&1
+echo "Cleaning up old build artifacts..."
+
+rm -rf $SELF_DIR/outputs >/dev/null 2>&1
+rm -rf $SELF_DIR/result >/dev/null 2>&1
+# Clear out the existing build artifacts as these aren't automatically removed by wasm-pack.
+if [ -d ./pkg/ ]; then
+    rm -rf $SELF_DIR/pkg/
+fi
+if [ -d ./dist/ ]; then
+    rm -rf $SELF_DIR/dist/
+fi
+mkdir dist
+
+cd src/rust-wasm
+PNAME=$(cargo read-manifest | jq -r '.name')
 
 if [ -v out ]; then
   echo "Will install package to $out (defined outside installPhase.sh script)"
 else
-  export out="$self_path/outputs/out"
+  out="$SELF_DIR/outputs/out"
   echo "Will install package to $out"
 fi
 
-run_or_fail $self_path/buildPhaseCargoCommand.sh
-run_or_fail $self_path/installPhase.sh
+TARGET=wasm32-unknown-unknown
+WASM_BINARY=$SELF_DIR/target/$TARGET/release/${PNAME}.wasm
 
-ln -s $out $self_path/result
+CJS_DIR=$SELF_DIR/dist/cjs/wasm/
+ESM_DIR=$SELF_DIR/dist/esm/wasm/
+CJS_WASM=${CJS_DIR}/${PNAME}_bg.wasm
+ESM_WASM=${ESM_DIR}/${PNAME}_bg.wasm
+
+# Build the new wasm package
+run_or_fail cargo build --lib --release --target $TARGET
+run_or_fail wasm-bindgen $WASM_BINARY --out-dir $CJS_DIR --typescript --target nodejs
+run_or_fail wasm-bindgen $WASM_BINARY --out-dir $ESM_DIR --typescript --target web
+run_if_available wasm-opt $CJS_WASM -o $CJS_WASM -O
+run_if_available wasm-opt $ESM_WASM -o $ESM_WASM -O
+
