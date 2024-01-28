8671db2d566877a68de27df75d9c50689ed4a169
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-01-28 18:59:21 -0500

    test: pedersenHash test scaffolding & testing data

---
+++ src/js/tests/pedersenHash.spec.ts
@@ -0,0 +1,31 @@
+import { describe, test } from "vitest";
+
+// Test data src: https://github.com/crate-crypto/rust-verkle/blob/231c9fec87ade64c2a4a4b0c9425288971708ce3/ffi_interface/src/lib.rs#L307
+describe.todo("pedersenHash", () => {
+  test("computes the expected pedersenHash for the zero address", () => {
+    const input = Uint8Array.from([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+    ]);
+    const expectedHash =
+      "bf101a6e1c8e83c11bd203a582c7981b91097ec55cbd344ce09005c1f26d1922";
+    // const computedHash = pedersenHash(input);
+    // expect(computedHash).toBe(expectedHash);
+  });
+
+  test("computes the expected pedersenHash for a given address", () => {
+    const input = Uint8Array.from([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+      58, 59, 60, 61, 62, 63, 64,
+    ]);
+
+    const expected_hash =
+      "76a014d14e338c57342cda5187775c6b75e7f0ef292e81b176c7a5a700273700";
+
+    // const computedHash = pedersenHash(input);
+    // expect(computedHash).toBe(expectedHash);
+  });
+});
