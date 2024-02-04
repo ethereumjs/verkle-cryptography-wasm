25c9bb2fa89ad090ce3449f3f6cc818f49599737
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-02-04 13:26:42 -0500

    fix: imports and run linting

--- src/js/banderwagon_bindings/index.ts
+++ src/js/banderwagon_bindings/index.ts
@@ -1,3 +1,2 @@
-export { Point } from './point.js'
-export { commitToPoly, batchMapToScalarField } from './point.js'
-export { pedersenHash } from './pedersen.js'
+export { commitToPoly, batchMapToScalarField, Point } from './point'
+export { pedersenHash } from './pedersen'
--- src/js/banderwagon_bindings/pedersen.ts
+++ src/js/banderwagon_bindings/pedersen.ts
@@ -1,14 +1,13 @@
-import { batchMapToScalarField } from ".";
-import { Point, commitToPoly } from "./point.js";
+import { Point, commitToPoly } from './point'
+
+import { batchMapToScalarField } from '.'
 export const pedersenHash = (input: Uint8Array): Uint8Array => {
   if (input.length > 255 * 16)
-    throw new Error(
-      `input byte length ${input.length} longer than max ${255 * 16}`
-    );
-  const elements = [];
+    throw new Error(`input byte length ${input.length} longer than max ${255 * 16}`)
+  const elements = []
   for (let i = 0; i < input.length; i = i + 16) {
-    elements.push(Point.fromBytes(input.subarray(i, i + 16)));
+    elements.push(Point.fromBytes(input.subarray(i, i + 16)))
   }
-  const field = batchMapToScalarField(elements);
-  return commitToPoly(field).toBytes();
-};
+  const field = batchMapToScalarField(elements)
+  return commitToPoly(field).toBytes()
+}
