194233839f86e8818438981e47178ab6fb2b72e7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-02-11 19:09:26 +0000

    add test to check for consistency between js and rust

--- src/js/tests/ffi.spec.ts
+++ src/js/tests/ffi.spec.ts
@@ -1,9 +1,9 @@
 import { bytesToHex } from '@ethereumjs/util'
 import { beforeAll, describe, expect, test } from 'vitest'
 
-import { Context } from '../verkleFFIBindings/index'
+import { Context, getTreeKeyHashJs, getTreeKeyJs } from '../verkleFFIBindings/index'
 
-describe('bindings', () => {
+describe.only('bindings', () => {
   let context: Context
   beforeAll(() => {
     context = new Context()
@@ -33,6 +33,33 @@ describe('bindings', () => {
     const expected = '0x76a014d14e338c57342cda5187775c6b75e7f0ef292e81b176c7a5a700273700'
 
     expect(key_hex).toBe(expected)
+  })
+
+  test('getTreeKeyJsMatchesRust', () => {
+    const address = new Uint8Array([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+      27, 28, 29, 30, 31, 32,
+    ])
+
+    const tree_index_le = new Uint8Array([
+      33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+      56, 57, 58, 59, 60, 61, 62, 63, 64,
+    ])
+
+    // Reverse the tree_index array so it is in little endian form
+    tree_index_le.reverse()
+
+    const sub_index = 0
+
+    const keyRust = context.getTreeKey(address, tree_index_le, sub_index)
+    const keyRustHex = bytesToHex(keyRust)
+
+    const keyJs = getTreeKeyJs(context, address, tree_index_le, sub_index)
+    const keyJsHex = bytesToHex(keyJs)
+
+    const expected = '0x76a014d14e338c57342cda5187775c6b75e7f0ef292e81b176c7a5a700273700'
+
+    expect(keyRustHex).toBe(keyJsHex)
   })
 
   test('commitToScalars', () => {
