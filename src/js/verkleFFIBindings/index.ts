148212a78548eba01067a1c95aea754215b90e18
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-02-11 16:45:13 +0000

    re-expose some more artifacts

--- src/js/verkleFFIBindings/index.ts
+++ src/js/verkleFFIBindings/index.ts
@@ -1,1 +1,18 @@
-export { ContextWrapper } from '../../../dist/cjs/wasm/rust_verkle_wasm'
+import { ContextWrapper, zeroCommitment } from '../../../dist/cjs/wasm/rust_verkle_wasm'
+
+// This is a 32 byte serialized field element
+type Scalar = Uint8Array
+
+// This is a 64 byte serialized point.
+// It is 64 bytes because the point is serialized in uncompressed format.
+type Commitment = Uint8Array
+
+export type Context = ContextWrapper
+
+export {
+  Scalar,
+  Commitment,
+  // This is a function that returns a zero commitment
+  // wasm_bindgen does not seem to allow returning constants
+  zeroCommitment,
+}
