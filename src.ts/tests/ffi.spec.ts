c7aaf1a25b4c96aafb5c2305b09e24b13869e249
Holger Drewes <Holger.Drewes@gmail.com>
2024-03-07 13:42:56 +0100

    Add test for getTreeKeyHash()

--- src.ts/tests/ffi.spec.ts
+++ src.ts/tests/ffi.spec.ts
@@ -1,7 +1,7 @@
 import { bytesToHex } from '@ethereumjs/util'
 import { beforeAll, describe, expect, test } from 'vitest'
 
-import { Context, getTreeKey } from '../verkleFFIBindings/index'
+import { Context, getTreeKey, getTreeKeyHash } from '../index'
 
 describe('bindings', () => {
   let context: Context
@@ -33,6 +33,28 @@ describe('bindings', () => {
     const expected = '0x76a014d14e338c57342cda5187775c6b75e7f0ef292e81b176c7a5a700273700'
 
     expect(keyHex).toBe(expected)
+  })
+
+  test('getTreeKeyHash', () => {
+    const address = new Uint8Array([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+      27, 28, 29, 30, 31, 32,
+    ])
+
+    const treeIndexLE = new Uint8Array([
+      33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+      56, 57, 58, 59, 60, 61, 62, 63, 64,
+    ])
+
+    // Reverse the tree index array so it is in little endian form
+    treeIndexLE.reverse()
+
+    const hash = getTreeKeyHash(context, address, treeIndexLE)
+    const hashHex = bytesToHex(hash)
+
+    const expected = '0x76a014d14e338c57342cda5187775c6b75e7f0ef292e81b176c7a5a70027373a'
+
+    expect(hashHex).toBe(expected)
   })
 
   test('getTreeKeyJsMatchesRust', () => {
