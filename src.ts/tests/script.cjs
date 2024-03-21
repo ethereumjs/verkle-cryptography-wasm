e7ff134edab67709c04f10918d70800e36c251bc
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-03-21 18:27:15 -0500

    adjust cjs test since we're not exporting Context

--- src.ts/tests/script.cjs
+++ src.ts/tests/script.cjs
@@ -1,9 +1,8 @@
-const { initVerkleWasm, Context, zeroCommitment } = require("../../dist/cjs/index");
+const { initVerkleWasm, zeroCommitment } = require('../../dist/cjs/index')
 
 const main = async () => {
-    await initVerkleWasm()
-    const ffi = new Context()
-    console.log(zeroCommitment())
+  await initVerkleWasm()
+  console.log(zeroCommitment())
 }
 
 main()
