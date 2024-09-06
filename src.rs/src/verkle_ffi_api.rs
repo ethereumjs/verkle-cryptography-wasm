c32db8fb6c74eb585e3be3f5fe1c6cf248941893
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-09-06 15:52:33 -0400

    Return the actual error and not false when proof verification fails

--- src.rs/src/verkle_ffi_api.rs
+++ src.rs/src/verkle_ffi_api.rs
@@ -215,11 +215,11 @@ impl Context {
     pub fn verify_proof(
         &self,
         input: Uint8Array,
-    ) -> Result<Boolean, Boolean>
+    ) -> Result<Boolean, JsError>
     {
         let input_bytes = serde_wasm_bindgen::from_value(input.into()).unwrap();
         let result = ffi_interface::verify_proof(&self.inner, input_bytes).map(|_op |Boolean::from(true))
-        .map_err(|_err| Boolean::from(false));
+        .map_err(|err| JsError::new(&format!("proof verification failed]: {:?}", err)));
         return result
     }
 }
